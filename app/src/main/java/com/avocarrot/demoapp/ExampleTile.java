69565df224d528231d3f9720940701c1d1219d04
nikos <nikos@avocarrot.com>
2015-05-11 14:25:37 +0300

    update demo app for avocarrot-sdk 3.3.1

--- app/src/main/java/com/avocarrot/demoapp/ExampleTile.java
+++ app/src/main/java/com/avocarrot/demoapp/ExampleTile.java
@@ -13,7 +13,6 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.avocarrot.androidsdk.instream.AvocarrotInstream;
 import com.avocarrot.demoapp.helpers.DemoContent;
 import com.avocarrot.demoapp.main.R;
 
